f94c79c55716b9cf92686ac699808211a34f8409
Gary Garrison <garrisong@gmail.com>
2026-02-17 18:53:37 -0700

    Initial Commit

---
+++ ModusMemori/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ModusMemori
+//
+//  Created by Gary Garrison on 2/17/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
